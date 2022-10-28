b8459269cb1c0d6afa708fb5c6192e72a3cc2c36
Muhammad Syarif H <42500655+msyrh@users.noreply.github.com>
2022-10-29 04:59:09 +0700

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDogen: false,
+    reactDogenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+};
