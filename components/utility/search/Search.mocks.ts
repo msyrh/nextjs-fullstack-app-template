7dc5f8158942c579ead21fa122ac331e36c7775a
Muhammad Syarif H <42500655+msyrh@users.noreply.github.com>
2022-10-30 16:59:31 +0700

    feat: create Search component

---
+++ components/utility/search/Search.mocks.ts
@@ -0,0 +1,10 @@
+/** Ini file untuk menyimpan data tiruan komponen dari */
+import { ISearch } from './Search';
+
+const base: ISearch = {
+  sampleTextProp: 'Hello world!',
+};
+
+export const mockSearchProps = {
+  base,
+};
