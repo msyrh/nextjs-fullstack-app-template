02ce76b0f4dfaabc468c2c95f7941cba833cb7b1
Muhammad Syarif H <42500655+msyrh@users.noreply.github.com>
2022-10-31 12:53:58 +0700

    feat: implement search API & and result page query

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -16,7 +16,7 @@ const PrimaryLayout: React.FC<IPrimaryLayout> = ({
       <Head>
         <title>NextJs Fullstack App Template</title>
       </Head>
-      <div {...divProps} className="min-h-screen flex flex-col items-center">
+      <div {...divProps} className={`min-h-screen flex flex-col ${justify}`}>
         <Header />
         <main className="px-5">{children}</main>
         <div className="m-auto" />
