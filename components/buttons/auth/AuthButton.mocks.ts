6d8577bd7ccf8572decdb07be06a80f2bf5c2864
Muhammad Syarif H <42500655+msyrh@users.noreply.github.com>
2022-10-31 15:24:13 +0700

    feat: implement sharing state between page

--- components/buttons/auth/AuthButton.mocks.ts
+++ components/buttons/auth/AuthButton.mocks.ts
@@ -1,9 +1,7 @@
 /** Ini file untuk menyimpan data tiruan komponen dari */
 import { IAuthButton } from './AuthButton';
 
-const base: IAuthButton = {
-  sampleTextProp: 'Hello world!',
-};
+const base: IAuthButton = {};
 
 export const mockAuthButtonProps = {
   base,
